4a08c610d937517af17cf6a288b1b23fa230cab5
TyphooN48 <andrew.burlackov@yandex.ru>
2024-01-04 15:10:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Store from './components/Store/Store';
+import { products } from './data/data'
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <Store products={ products }/>
+    </div>
+  );
+}
+
+export default App;
